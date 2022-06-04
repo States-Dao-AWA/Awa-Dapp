6083b81f3276661f6b94ce03c3d789f675a605aa
SeungWookHan <hswook12@me.com>
2022-06-05 08:47:07 +0900

    Add basic input form

--- near/frontend/App.js
+++ near/frontend/App.js
@@ -1,76 +1,66 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
+import "regenerator-runtime/runtime";
+import React from "react";
 
-import './assets/css/global.css'
+import "./assets/css/global.css";
 
-import {login, logout, get_greeting, set_greeting} from './assets/js/near/utils'
-import getConfig from './assets/js/near/config'
-
+import {
+  login,
+  logout,
+  get_greeting,
+  set_greeting,
+} from "./assets/js/near/utils";
+import getConfig from "./assets/js/near/config";
 
 export default function App() {
   // use React Hooks to store greeting in component state
-  const [greeting, setGreeting] = React.useState()
+  const [greeting, setGreeting] = React.useState();
 
   // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+  const [buttonDisabled, setButtonDisabled] = React.useState(true);
 
   // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false)
+  const [showNotification, setShowNotification] = React.useState(false);
 
   // The useEffect hook can be used to fire side-effects during render
   // Learn more: https://reactjs.org/docs/hooks-intro.html
   React.useEffect(
     () => {
       // get_greeting is in near/utils.js
-      get_greeting()
-        .then(greetingFromContract => {
-          setGreeting(greetingFromContract)
-        })
+      get_greeting().then((greetingFromContract) => {
+        setGreeting(greetingFromContract);
+      });
     },
 
     // The second argument to useEffect tells React when to re-run the effect
     // Use an empty array to specify "only run on first render"
     // This works because signing into NEAR Wallet reloads the page
     []
-  )
+  );
+
+  function handleMyInput(event) {
+    const { value, max } = event.target;
+    if (value >= max) alert("Max value reached");
+  }
 
   // if not signed in, return early with sign-in prompt
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)'
-            }}
-          >
-            {greeting}
-          </label>!
-          Welcome to NEAR!
-        </h1>
-        <p>
-        Your contract is storing a greeting message in the NEAR blockchain. To
-        change it you need to sign in using the NEAR Wallet. It is very simple,
-        just use the button below.
-        </p>
-        <p>
-        Do not worry, this app runs in the test network ("testnet"). It works
-        just like the main network ("mainnet"), but using NEAR Tokens that are
-        only for testing!
-        </p>
-        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
+        <h1>Crypto projects are reducing XXX amount of CO2 emmision / day</h1>
+        <form>
+          <input type="number" name="guess" min="0" max="10000" />
+        </form>
+        <p style={{ textAlign: "center", marginTop: "2.5em" }}>
           <button onClick={login}>Sign in</button>
         </p>
       </main>
-    )
+    );
   }
 
   return (
     // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
     <>
-      <button className="link" style={{ float: 'right' }} onClick={logout}>
+      <button className="link" style={{ float: "right" }} onClick={logout}>
         Sign out
       </button>
       <main>
@@ -78,119 +68,151 @@ export default function App() {
           <label
             htmlFor="greeting"
             style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)'
-            }}
-          >
+              color: "var(--secondary)",
+              borderBottom: "2px solid var(--secondary)",
+            }}>
             {greeting}
           </label>
-          {' '/* React trims whitespace around tags; insert literal space character when needed */}
+          {
+            " " /* React trims whitespace around tags; insert literal space character when needed */
+          }
           {window.accountId}!
         </h1>
-        <form onSubmit={async event => {
-          event.preventDefault()
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault();
 
-          // get elements from the form using their id attribute
-          const { fieldset, greeting } = event.target.elements
+            // get elements from the form using their id attribute
+            const { fieldset, greeting } = event.target.elements;
 
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-          const newGreeting = greeting.value
+            // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
+            const newGreeting = greeting.value;
 
-          // disable the form while the value gets updated on-chain
-          fieldset.disabled = true
+            // disable the form while the value gets updated on-chain
+            fieldset.disabled = true;
 
-          try {
-            // make an update call to the smart contract
-            // pass the value that the user entered in the greeting field
-            await set_greeting(newGreeting)
-          } catch (e) {
-            alert(
-              'Something went wrong! ' +
-              'Maybe you need to sign out and back in? ' +
-              'Check your browser console for more info.'
-            )
-            throw e
-          } finally {
-            // re-enable the form, whether the call succeeded or failed
-            fieldset.disabled = false
-          }
+            try {
+              // make an update call to the smart contract
+              // pass the value that the user entered in the greeting field
+              await set_greeting(newGreeting);
+            } catch (e) {
+              alert(
+                "Something went wrong! " +
+                  "Maybe you need to sign out and back in? " +
+                  "Check your browser console for more info."
+              );
+              throw e;
+            } finally {
+              // re-enable the form, whether the call succeeded or failed
+              fieldset.disabled = false;
+            }
 
-          // update local `greeting` variable to match persisted value
-          setGreeting(newGreeting)
+            // update local `greeting` variable to match persisted value
+            setGreeting(newGreeting);
 
-          // show Notification
-          setShowNotification(true)
+            // show Notification
+            setShowNotification(true);
 
-          // remove Notification again after css animation completes
-          // this allows it to be shown again next time the form is submitted
-          setTimeout(() => {
-            setShowNotification(false)
-          }, 11000)
-        }}>
+            // remove Notification again after css animation completes
+            // this allows it to be shown again next time the form is submitted
+            setTimeout(() => {
+              setShowNotification(false);
+            }, 11000);
+          }}>
           <fieldset id="fieldset">
             <label
               htmlFor="greeting"
               style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
-              }}
-            >
+                display: "block",
+                color: "var(--gray)",
+                marginBottom: "0.5em",
+              }}>
               Change greeting
             </label>
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: "flex" }}>
               <input
                 autoComplete="off"
                 defaultValue={greeting}
                 id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
+                onChange={(e) => setButtonDisabled(e.target.value === greeting)}
                 style={{ flex: 1 }}
               />
               <button
                 disabled={buttonDisabled}
-                style={{ borderRadius: '0 5px 5px 0' }}
-              >
+                style={{ borderRadius: "0 5px 5px 0" }}>
                 Save
               </button>
             </div>
           </fieldset>
         </form>
         <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
+          Look at that! A Hello World app! This greeting is stored on the NEAR
+          blockchain. Check it out:
         </p>
         <ol>
           <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>get_greeting</code> and <code>set_greeting</code> being called on <code>contract</code>. What's this?
+            Look in <code>src/App.js</code> and <code>src/utils.js</code> –
+            you'll see <code>get_greeting</code> and <code>set_greeting</code>{" "}
+            being called on <code>contract</code>. What's this?
           </li>
           <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/develop/contracts/overview">smart contract</a>.</li>
+            Ultimately, this <code>contract</code> code is defined in{" "}
+            <code>assembly/main.ts</code> – this is the source code for your{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://docs.near.org/docs/develop/contracts/overview">
+              smart contract
+            </a>
+            .
+          </li>
           <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
+            When you run <code>yarn dev</code>, the code in{" "}
+            <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You
+            can see how this happens by looking in <code>package.json</code> at
+            the <code>scripts</code> section to find the <code>dev</code>{" "}
+            command.
+          </li>
         </ol>
         <hr />
         <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
+          To keep learning, check out{" "}
+          <a target="_blank" rel="noreferrer" href="https://docs.near.org">
+            the NEAR docs
+          </a>{" "}
+          or look through some{" "}
+          <a target="_blank" rel="noreferrer" href="https://examples.near.org">
+            example apps
+          </a>
+          .
         </p>
       </main>
       {showNotification && <Notification />}
     </>
-  )
+  );
 }
 
 // this component gets rendered by App after the form is submitted
 function Notification() {
-  const { networkId } = getConfig(process.env.NODE_ENV || 'development')
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
+  const { networkId } = getConfig(process.env.NODE_ENV || "development");
+  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`;
 
   return (
     <aside>
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`${urlPrefix}/${window.accountId}`}>
         {window.accountId}
       </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
-      called method: 'set_greeting' in contract:
-      {' '}
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
+      {
+        " " /* React trims whitespace around tags; insert literal space character when needed */
+      }
+      called method: 'set_greeting' in contract:{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`${urlPrefix}/${window.contract.contractId}`}>
         {window.contract.contractId}
       </a>
       <footer>
@@ -198,5 +220,5 @@ function Notification() {
         <div>Just now</div>
       </footer>
     </aside>
-  )
+  );
 }
