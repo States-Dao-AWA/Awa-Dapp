583e6ba2c0d92e0e29c300f8cb865f6a6da1f402
yun-sangho <contact@yunsangho.com>
2022-06-05 20:28:06 +0900

    feat: Create contract handle user's guessing

--- near/contract/src/lib.rs
+++ near/contract/src/lib.rs
@@ -1,72 +1,77 @@
-/*
- * Example smart contract written in RUST
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://near-docs.io/develop/Contract
- *
- */
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
-
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
+use near_sdk::{near_bindgen, AccountId, Timestamp, BorshStorageKey,};
+use near_sdk::collections::LookupMap;
 
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    message: String,
+    pub owner_id: AccountId,
+    //keeps track of all the token IDs for a given account
+    pub guesses_per_owner: LookupMap<AccountId, Timestamp>,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{message: DEFAULT_MESSAGE.to_string()}
+#[derive(BorshStorageKey, BorshSerialize)]
+pub enum StorageKey {
+    GuessByOwnerId,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        near_sdk::env::panic_str("Contract should be initialized before the usage.")
     }
 }
 
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        let this = Self {
+            guesses_per_owner: LookupMap::new(StorageKey::GuessByOwnerId),
+            owner_id,
+        };
+
+        this
     }
 
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
+    pub fn get_user_last_guess(&self, owner_id: AccountId) -> Option<Timestamp> {
+        return self.guesses_per_owner.get(&owner_id);
+    }
+
+    pub fn set_user_last_guess(&mut self, owner_id: AccountId, timestamp: Timestamp) {
+        self.guesses_per_owner.insert(&owner_id, &timestamp);
     }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
+#[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::{testing_env};
+
     use super::*;
 
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
     }
 
     #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
+    fn test_new() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let mut contract = Contract::new(accounts(1).into());
+
+        assert_eq!(contract.get_user_last_guess(accounts(1).into()), None);
+
+        contract.set_user_last_guess(accounts(1).into(), 1654414023158);
+        assert_eq!(contract.get_user_last_guess(accounts(1).into()).unwrap(), 1654414023158);
+
+        contract.set_user_last_guess(accounts(1).into(), 1654414023200);
+        assert_eq!(contract.get_user_last_guess(accounts(1).into()).unwrap(), 1654414023200);
     }
 }
