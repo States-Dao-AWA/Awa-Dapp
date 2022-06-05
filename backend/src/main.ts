e02eb81377f0f3aad75487687dece85be39f73c4
steve <ionc635@naver.com>
2022-06-05 05:44:51 -0700

    fix: Fix error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  //
-  // const response = await (contract as any).get_user_last_guess({
-  //   owner_id: 'app2.coza.testnet',
-  // });
-
-  // console.log(response, '!!!!!!!!!!!!!!!');
-  // await (contract as any).ft_transfer({
-  //   receiver_id: 'yunsangho.testnet',
-  //   gas: '300000000000000',
-  //   amount: '10000000000000',
-  // }); // attached deposit in yoctoNEAR (optional));
-
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   await app.listen(3000);
 }
 
