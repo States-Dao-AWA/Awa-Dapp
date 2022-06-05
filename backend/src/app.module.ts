12d6245d7e4a55e455320071ed94fd2222d23fd2
steve <ionc635@naver.com>
2022-06-04 23:38:06 -0700

    add: Install config package & set config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GuessModule } from './guess/guess.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GuessModule],
+  imports: [
+    GuessModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.prod.env' : '.dev.env',
+    }),
+  ],
 })
 export class AppModule {}
